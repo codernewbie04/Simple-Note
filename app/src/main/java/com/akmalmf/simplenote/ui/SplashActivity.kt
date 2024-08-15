be2746251393f34368eb5e18626a67e894777579
Akmal Muhamad Firdaus <coder.newbie04@gmail.com>
2024-08-15 16:59:43 +0700

    feat: SplashScreen

---
+++ app/src/main/java/com/akmalmf/simplenote/ui/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.akmalmf.simplenote.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.akmalmf.simplenote.MainActivity
+import com.akmalmf.simplenote.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        lifecycleScope.launch {
+            delay(1500)
+            // Pause the coroutine for 1.5 seconds to display the splash screen before moving to the main activity
+            MainActivity.start(this@SplashActivity)
+            finish()
+            // Finish the current activity (make sure the user can't go back to the splash screen)
+        }
+    }
+}
